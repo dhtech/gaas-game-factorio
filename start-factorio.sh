2b975cae317459ead3bd3ac689e2c9ab1d11705b
Christian Svensson <blue@cmd.nu>
2019-06-15 15:35:25 +0200

    Import

---
+++ start-factorio.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+BIN=./bin/x64/factorio
+SAVES_DIR=/factorio/saves
+SAVE=$SAVES_DIR/save.zip
+
+[[ -f $SAVE ]] && EXISTING_SAVE="yes"
+[[ -f $SAVE ]] || $BIN --create $SAVE
+
+JQ_FILTER=""
+
+[[ -n $SERVER_NAME ]] && JQ_FILTER="$JQ_FILTER .name = \"$SERVER_NAME\" |"
+[[ -n $SERVER_DESCRIPTION ]] && JQ_FILTER="$JQ_FILTER .description = \"$SERVER_DESCRIPTION\" |"
+
+JQ_FILTER="$JQ_FILTER ."
+
+jq "$JQ_FILTER" data/server-settings.example.json > data/server-settings.json
+
+exec $BIN \
+  --start-server-load-latest \
+  --server-settings /factorio/data/server-settings.json
+  --bind "${IP_ALLOC}" \
+  --no-log-rotation
